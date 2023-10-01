839484d06b622c247575637de5c694ebc7422ae8
Thethickinnchickin <matt.reiley@sjsu.edu>
2023-10-01 01:58:58 -0700

    a

--- my-app/pages/api/index.js
+++ my-app/pages/api/index.js
@@ -8,8 +8,7 @@ const openai = new OpenAI({
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const body = await req.body;
-    console.log("Hey Matt");
-    console.log(body);
+
 
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
@@ -17,6 +16,7 @@ export default async function handler(req, res) {
     });
     
     const theResponse = completion.choices[0].message;
+    console.log(theResponse)
 
     res.status(200).json({ output: theResponse });
   } else {
